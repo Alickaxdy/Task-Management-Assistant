cb2a36859814ccd980120565a458d992e9397f9a
Alick Li <ss0852800821@gmail.com>
2023-11-18 21:29:05 -0500

    cal.ui

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from PyQt6 import QtCore, QtGui, QtWidgets, uic
+from PyQt6.QtWidgets import *
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("cal.ui", self)
+        self.calendar = self.findChild(QCalendarWidget, "calendar")
+        self.label = self.findChild(QLabel, "label")
+        self.calendar.selectionChanged.connect(self.grab_date)
+        self.show()
+    def grab_date(self):
+        dateSelected = self.calendar.selectedDate()
+        self.label.setText(str(dateSelected.toPyDate()))
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec()
